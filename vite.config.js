d7e56b96914fe1a428b09909468824c68479247d
euleolima <hello.leonardolmonteiro@gmail.com>
2026-06-21 16:24:37 -0300

    lume v1.0

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  base: '/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icons/*.png', 'favicon.svg'],
+      manifest: {
+        name: 'Lume',
+        short_name: 'Lume',
+        description: 'Mantenha aceso.',
+        theme_color: '#0F0C0A',
+        background_color: '#0F0C0A',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        scope: '/',
+        lang: 'pt-BR',
+        icons: [
+          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
+          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}']
+      }
+    })
+  ]
+})
